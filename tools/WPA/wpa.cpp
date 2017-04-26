993dbd4c52a37a145d930ec69e9e438572785a77
Will Dietz <w@wdtz.org>
2017-04-26 07:50:25 -0500

    wpa: don't write bitcode back to disk
    At least in my use of the tool the IR doesn't
    seem to be changed so this step is unnecessary.
    
    Not worth disabling if that was the whole story,
    but as-is the output path is hardcoded and breaks
    the ability to analyze IR stored in a read-only directory.

--- tools/WPA/wpa.cpp
+++ tools/WPA/wpa.cpp
@@ -30,16 +30,12 @@
 
 #include <llvm-c/Core.h> // for LLVMGetGlobalContext()
 #include <llvm/Support/CommandLine.h>	// for cl
-#include <llvm/Support/FileSystem.h>	// for sys::fs::F_None
-#include <llvm/Bitcode/BitcodeWriterPass.h>  // for bitcode write
 #include <llvm/IR/LegacyPassManager.h>		// pass manager
 #include <llvm/Support/Signals.h>	// singal for command line
 #include <llvm/IRReader/IRReader.h>	// IR reader for bit file
-#include <llvm/Support/ToolOutputFile.h> // for tool output file
 #include <llvm/Support/PrettyStackTrace.h> // for pass list
 #include <llvm/IR/LLVMContext.h>		// for llvm LLVMContext
 #include <llvm/Support/SourceMgr.h> // for SMDiagnostic
-#include <llvm/Bitcode/BitcodeWriterPass.h>		// for createBitcodeWriterPass
 
 
 using namespace llvm;
@@ -55,8 +51,6 @@ int main(int argc, char ** argv) {
 
     LLVMOpaqueContext * WrappedContextRef = LLVMGetGlobalContext();
     LLVMContext &Context = *unwrap(WrappedContextRef);
-
-    std::string OutputFilename;
 
     cl::ParseCommandLineOptions(argc, argv, "Whole Program Points-to Analysis\n");
     sys::PrintStackTraceOnErrorSignal(argv[0]);
@@ -84,29 +78,9 @@ int main(int argc, char ** argv) {
         return 1;
     }
 
-
-    std::unique_ptr<tool_output_file> Out;
-    std::error_code ErrorInfo;
-
-    StringRef str(InputFilename);
-    InputFilename = str.rsplit('.').first;
-    OutputFilename = InputFilename + ".wpa";
-
-    Out.reset(
-        new tool_output_file(OutputFilename.c_str(), ErrorInfo,
-                             sys::fs::F_None));
-
-    if (ErrorInfo) {
-        errs() << ErrorInfo.message() << '\n';
-        return 1;
-    }
-
     Passes.add(new WPAPass());
 
-    Passes.add(createBitcodeWriterPass(Out->os()));
-
     Passes.run(*M1.get());
-    Out->keep();
 
     return 0;
 
